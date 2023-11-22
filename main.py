13d79b35a246d09b4cf6faa1d2b439cc3608154f
unknown <k.moksud@gmail.com>
2023-11-22 15:02:10 +0600

    updated API key

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, jsonify, request
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
 
 app = Flask(__name__)
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = 'AIzaSyCACYSikOdigMLUR2dBejH3o_lRTc3VePE'
+DEVELOPER_KEY = os.getenv("API_KEY")
+debug_mode = os.getenv("DEBUG")
+
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
